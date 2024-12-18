dbc70e6d1e36cc500797ba39b9d6dd240ee83969
Sebastian Bullinger <sebastian.bullinger@iosb.fraunhofer.de>
2024-12-18 12:58:39 +0100

    Discard patch files where only the index has changed

--- create_patches.sh
+++ create_patches.sh
@@ -25,6 +25,43 @@ patch_dp="${sh_file_dp}/patches"
 echo "Creating patch files in: $patch_dp"
 
 cd $modified_colmap_source_dp
+git branch
+
+reset_file_if_only_index_changes() {
+    local patch_fp=$1
+    # Switch to the repository with the patch files, and check
+    # if there are substantial changes in the patch file
+    cd $sh_file_dp
+
+    # shortstat_result=$(git diff --shortstat $patch_fp)
+    numstat_result=$(git diff --numstat $patch_fp)
+    read -r num_added_lines num_deleted_lines fn <<< "$numstat_result"
+
+    if [ $num_added_lines == 1 ] && [ $num_deleted_lines == 1 ]; then
+        local diff_output=$(git diff -- "$patch_fp")
+        # printf "%s\n" "$diff_output"
+
+        # Check for lines that start with "+index"" or "-index"
+        local added_index_lines=$(echo "$diff_output" | grep '^+index')
+        local removed_index_lines=$(echo "$diff_output" | grep '^-index')
+        local num_added_index_lines=$(echo "$added_index_lines" | wc -l)
+        local num_removed_index_lines=$(echo "$removed_index_lines" | wc -l)
+
+        if [ $num_added_index_lines == 1 ] && [ $num_removed_index_lines == 1 ]; then
+            # echo "Only index lines have changed in $patch_fp"
+            git restore $patch_fp
+        else
+            echo "ERROR: SINGLE CHANGE (BUT NOT INDEX LINE) in $patch_fp"
+            exit
+        fi
+    # else
+    #     echo "Actual changes in $patch_fp"
+    fi
+
+    # Switch back to the previous directory (i.e. the colmap git repository)
+    cd $modified_colmap_source_dp
+}
+
 
 create_patch() {
     local source_fp=$1
@@ -32,6 +69,7 @@ create_patch() {
     local patch_fp="$patch_dp/$patch_fn"
     if [ "$overwrite_patch_file" -eq 1 ] || [ ! -f "$patch_fp" ]; then
         git diff "$source_fp" > "$patch_fp"
+        reset_file_if_only_index_changes $patch_fp
     fi
 }
 
