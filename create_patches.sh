feaef73c72a0eec67297cff528c5d4910e560ba1
Sebastian Bullinger <sebastian.bullinger@iosb.fraunhofer.de>
2025-01-15 10:25:59 +0100

    Derive patch file names from actual changes in repository

--- create_patches.sh
+++ create_patches.sh
@@ -91,44 +91,24 @@ create_patch() {
     fi
 }
 
-create_patch src/base/camera.cc "src__base__camera.cc.patch"
-create_patch src/base/camera_models.h "src__base__camera_models.h.patch"
-create_patch src/base/cost_functions.h "src__base__cost_functions.h.patch"
-create_patch src/base/database_cache.h "src__base__database_cache.h.patch"
-create_patch src/base/reconstruction.cc "src__base__reconstruction.cc.patch"
-create_patch src/base/reconstruction.h "src__base__reconstruction.h.patch"
+encode_path_as_filename() {
+    local filepath="$1"
+    echo "${filepath//\//__}"
+}
 
-create_patch src/controllers/bundle_adjustment.cc "src__controllers__bundle_adjustment.cc.patch"
-# Normalization has been moved from src/controllers/bundle_adjustment.cc to src/ui/bundle_adjustment_widget.cc
-create_patch src/ui/bundle_adjustment_widget.cc "src__ui__bundle_adjustment_widget.cc.patch"
+decode_filename_as_path() {
+    local filename="$1"
+    echo "${filename//__/\/}"
+}
 
-# src/base/colmap.cc has been reorganized into src/base/colmap.cc, src/base/sfm.cc, ...
-create_patch src/exe/colmap.cc "src__exe__colmap.cc.patch"
-create_patch src/exe/sfm.cc "src__exe__sfm.cc.patch"
-create_patch src/exe/sfm.h "src__exe__sfm.h.patch"
+git_diff_files=$(git diff --name-only)
 
-create_patch src/feature/sift.cc "src__feature__sift.cc.patch"
-
-create_patch src/mvs/depth_map.cc "src__mvs__depth_map.cc.patch"
-create_patch src/mvs/depth_map.h "src__mvs__depth_map.h.patch"
-create_patch src/mvs/fusion.cc "src__mvs__fusion.cc.patch"
-create_patch src/mvs/fusion.h "src__mvs__fusion.h.patch"
-create_patch src/mvs/image.cc "src__mvs__image.cc.patch"
-create_patch src/mvs/image.h "src__mvs__image.h.patch"
-# src/mvs/mat.h has no actual changes
-create_patch src/mvs/model.cc "src__mvs__model.cc.patch"
-create_patch src/mvs/model.h "src__mvs__model.h.patch"
-create_patch src/mvs/patch_match.cc "src__mvs__patch_match.cc.patch"
-create_patch src/mvs/patch_match.h "src__mvs__patch_match.h.patch"
-create_patch src/mvs/patch_match_cuda.cu "src__mvs__patch_match_cuda.cu.patch"
-create_patch src/mvs/patch_match_cuda.h "src__mvs__patch_match_cuda.h.patch"
-create_patch src/mvs/workspace.h "src__mvs__workspace.h.patch"
-
-create_patch src/optim/bundle_adjustment.cc "src__optim__bundle_adjustment.cc.patch"
-create_patch src/optim/bundle_adjustment.h "src__optim__bundle_adjustment.h.patch"
-
-create_patch src/sfm/incremental_mapper.cc "src__sfm__incremental_mapper.cc.patch"
-
-create_patch src/util/option_manager.cc "src__util__option_manager.cc.patch"
+for file_path in $git_diff_files; do
+  # Process each file (e.g., print its name or perform some action)
+  echo "File with changes: $file_path"
+  file_path_encoded=$(encode_path_as_filename $file_path)
+  patch_file_name="${file_path_encoded}.patch"
+  create_patch $file_path $patch_file_name
+done
 
 cd $original_dp
